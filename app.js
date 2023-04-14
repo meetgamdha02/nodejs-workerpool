26306e4536fe2c02afa217628367846cb3002f82
Meet Gamdha <42187294+meetgamdha02@users.noreply.github.com>
2023-04-14 12:47:44 +0000

    init commit

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 const app = require("express")();
 const workerpool = require("workerpool");
-const pool = workerpool.pool();
 const _ = require("lodash");
+const axios = require("axios");
 
-const heavyTask = async () => {
-  let count = 0;
-  let i = 0;
-  console.log(`hererererer`);
-  while (i < 100000) {
-    i++;
-    count++;
-  }
-
-  return count;
-};
+const pool = workerpool.pool(__dirname + "/worker.js");
 
 app.get("/", (req, res, next) => {
   res.send("Hi Meet");
 });
+
+const heavyTask2 = async () => {
+  try {
+    const requestData = await axios.get("https://swapi.dev/api/people/1");
+    const result = requestData.data;
+    return result;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const promiseHandlingFunction = async (params) => {
   const { shouldUsePool = true } = params;
   const promiseArr = [];
   for (let i = 0; i < 10; i++) {
     if (shouldUsePool) {
-      promiseArr.push(pool.exec(heavyTask));
+      promiseArr.push(pool.exec("heavyTask"));
     } else {
-      promiseArr.push(heavyTask());
+      promiseArr.push(heavyTask2());
     }
   }
 
@@ -50,7 +50,6 @@ app.get("/heavy", async (req, res) => {
 });
 
 app.get("/heavy2", async (req, res) => {
-  console.log(workerpool.cpus);
   console.time(`start`);
   try {
     const temp = await promiseHandlingFunction({ shouldUsePool: false });
